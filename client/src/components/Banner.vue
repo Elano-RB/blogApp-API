778b4282b390c3767c8c650fcd2cceb6b75ec8e9
RBElano <rbenjaminelano@gmail.com>
2025-12-05 21:13:34 +0800

    Refactored banner

--- client/src/components/Banner.vue
+++ client/src/components/Banner.vue
@@ -1,28 +1,22 @@
-<script>
-
-  export default {
-
-    props: {
-
-      bannerProps: Object
-    },
-
-  }
-</script>
-
 <template>
-  <div class="container-fluid">
-    <div class="row mb-5 py-5" id="banner">
-      <div class="col-md-10 p-5">
-        <h1 class="mb-3" id="banner-title">{{bannerProps.title}}</h1>
-        <p class="mb-5" id="motto">
-          {{bannerProps.tagline}}
-        </p>
-        <router-link class="btn btn-info btn-lg text-white rounded-pill px-3" :to="{ name: bannerProps.destination }">{{bannerProps.buttonLabel}} <i class="bi bi-arrow-right"></i></router-link>
-      </div>
+  <div id="banner">
+    <div class="banner-content">
+      <h1 id="banner-title">{{ bannerProps.title }}</h1>
+      <p id="motto">{{ bannerProps.tagline }}</p>
+      <router-link 
+        class="btn btn-info btn-lg text-white rounded-pill px-3" 
+        :to="{ name: bannerProps.destination }">
+        {{ bannerProps.buttonLabel }} <i class="bi bi-arrow-right"></i>
+      </router-link>
     </div>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  bannerProps: Object
+});
+</script>
 
 <style scoped>
   h1 {
